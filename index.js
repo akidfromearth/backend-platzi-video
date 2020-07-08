51e0ac1f24ec54c4077d7128edda1f1ec712baff
sntwandy <sntwandy@gmail.com>
2020-07-08 01:41:11 -0400

    Creating the API, requesting all data using express.router

--- index.js
+++ index.js
@@ -2,23 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config');
+const moviesApi = require('./routes/movies');
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
-app.get('/json', function (req, res) {
-  res.json( { hello: 'World!' } );
-});
-
-app.get('/:year', function (req, res) {
-  const year = req.params.year;
-  if (year % 4 == 0 || year % 100 == 0 || year % 400 == 0) {
-    res.send('Leap YEAR, has 366 days');
-  } else {
-    res.send('Sorry, not leap year!')
-  };
-});
+moviesApi(app);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
