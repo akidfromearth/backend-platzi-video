0fc69d393df2c938689796f69f75716eaba9cc6a
sntwandy <sntwandy@gmail.com>
2020-07-09 18:33:44 -0400

    Handling error 404 and other errors with boom

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const app = express();
 
 const { config } = require('./config');
 const moviesApi = require('./routes/movies');
-const { logErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
+const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 
 // Middleware Body Parser
 app.use(express.json());
 
 moviesApi(app);
 
+// Catch 404 error
+app.use(notFoundHandler);
+
 // Middlewares about errors was at the end.
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
+
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
--- utils/middlewares/errorHandlers.js
+++ utils/middlewares/errorHandlers.js
@@ -1,10 +1,18 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
-    return { error, stack };
+    return { ...error, stack };
   }
   return error;
+}
+
+function wrapErrors(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err));
+  };
+  next(err);
 }
 
 function logErrors(err, req, res,next) {
@@ -13,11 +21,13 @@ function logErrors(err, req, res,next) {
 };
 
 function errorHandler(err, req, res, next) { // eslint-disable-line
-  res.status(err.status || 500);
-  res.json(withErrorStack(err.message, err.stack));
+  const { output: { statusCode, payload } } = err;
+  res.status(statusCode);
+  res.json(withErrorStack(payload, err.stack));
 };
 
 module.exports = {
   logErrors,
+  wrapErrors,
   errorHandler
 }
